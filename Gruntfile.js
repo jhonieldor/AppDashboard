1f3e05e1b36d163657bfd1c2d25fc2b228db711d
jhonieldor <jhonieldor@gmail.com>
2017-03-30 16:40:47 -0300

    Ajustes para deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,7 +411,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif,txt,json}',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
