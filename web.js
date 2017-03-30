88c8a8475552d519592e45dc38f43a16fb14b1cc
jhonieldor <jhonieldor@gmail.com>
2017-03-30 16:18:32 -0300

    Ajustes nas dependências para deploy no heroku

--- web.js
+++ web.js
@@ -1,17 +1,8 @@
+var gzippo = require('gzippo');
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var PORT = process.env.PORT || 3000;
-var FACTORIAL = path.join(__dirname, 'build', 'factorial.min.js');
+var morgan = require('morgan');
+var app = express();
 
-express()
-  .use(hello)
-  .listen(PORT, onListen);
-
-function hello(req, res, next) {
-  res.sendFile(FACTORIAL);
-}
-
-function onListen() {
-  console.log('Listening on', PORT);
-}
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
