cebc2cf4e0009f53b472b787ec2d9236a1ad6d64
jhonieldor <jhonieldor@gmail.com>
2017-03-31 17:54:28 -0300

    Ajustes de html

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,7 +7,7 @@
  * # AboutCtrl
  * Controller of the appDashboardApp
  */
-angular.module('appDashboardApp')
+angular.module('ultraBI')
   .controller('AboutCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
